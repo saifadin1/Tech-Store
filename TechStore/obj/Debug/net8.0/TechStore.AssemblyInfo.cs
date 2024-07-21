2a161867d4b3f7ccec9714cf7e0c08f9f7f7700d
Saif Medhat <seifmedhatfarouk@gmail.com>
2024-07-21 03:59:51 +0300

    add view for Brand's details action

--- TechStore/obj/Debug/net8.0/TechStore.AssemblyInfo.cs
+++ TechStore/obj/Debug/net8.0/TechStore.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TechStore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+110944077af05c26ce807faa7bc2460748e4e280")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bb06fe225e963c5fb5555d89e4f81083bbec94e0")]
 [assembly: System.Reflection.AssemblyProductAttribute("TechStore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TechStore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
